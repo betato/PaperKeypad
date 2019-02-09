e80b9b2f148b37f2287af8f993da5e5963a3afd5
betato <battlestargatticus@gmail.com>
2019-02-09 13:51:30 -0500

    Add camera permission request

--- Android/app/src/main/java/org/betato/paperkeypad/MainActivity.java
+++ Android/app/src/main/java/org/betato/paperkeypad/MainActivity.java
@@ -1,5 +1,9 @@
 package org.betato.paperkeypad;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
@@ -17,9 +21,13 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
 
         cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
